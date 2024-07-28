5fb14460991fccd120da4e101c98c2403d8e562c
Ruby <ruby0322@ntu.im>
2024-07-28 18:22:28 +0800

    feat: type.d.ts

---
+++ type.d.ts
@@ -0,0 +1,15 @@
+import { Tables } from "@/types/supabase";
+
+export type ItemsTable = Tables<"items">;
+export type ParamsTable = Tables<"params">;
+export type RecommendationsTable = Tables<"recommendations">;
+export type SuggestionsTable = Tables<"suggestions">;
+export type UploadsTable = Tables<"uploads">;
+export type ResultsTable = Tables<"results">;
+
+export interface Recommendation {
+  params: ParamsTable;
+  items: {
+    [style: string]: ItemsTable[];
+  };
+}
