99cbe80641aa14c8091db3b8f95bf7abd08c1e51
mchen127 <mchen127.p@gmail.com>
2024-07-28 19:44:41 +0800

    uitls.ts

--- actions/utils.ts
+++ actions/utils.ts
@@ -10,11 +10,38 @@ const openai = new OpenAIApi(configuration);
 const chatCompletionTextAndImage = async (model: string, prompt: string, image_url: string): Promise<string> => {
     /* TODO: Using OpenAI API to get your response with a text and an image input.
        This server action purely takes your input and sends to GPT model to get your response. */
+    try {
+        const completion = await openai.chat.completions.create({
+            model: model,
+            messages: [{ role: "user", content: [
+                { type: "text", text: prompt },
+                { type: "image_url", image_url: image_url },
+            ] }],
+        });
+        const response = completion.choices[0];
+        return response;
+    } catch (e) {
+        console.log("Failed to get response from GPT API.");
+        console.log(e);
+        return "";
+    };
 };
 
 const chatCompletionTextOnly = async (model: string, prompt: string): Promise<string> => {
     /* TODO: Using OpenAI API to get your response with only a text input.
        This server action purely takes your input and sends to GPT model to get your response. */
+    try {
+        const completion = await openai.chat.completions.create({
+            model: model,
+            messages: [{ role: "user", content: prompt }],
+        });
+        const response = completion.choices[0];
+        return response;
+    } catch (e) {
+        console.log("Failed to get response from GPT API.");
+        console.log(e);
+        return "";
+    };
 };
 
 export { chatCompletionTextAndImage, chatCompletionTextOnly };
