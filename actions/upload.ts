a178457127ef74d09c2ebcd18be7295d887273e7
b10705033 <b10705033@ntu.im>
2024-11-11 15:52:12 +0800

    feat: backend semantic search

--- actions/upload.ts
+++ actions/upload.ts
@@ -1,5 +1,5 @@
 "use server";
-import { RecommendationWithoutLogin, SearchResult, UnstoredResult } from "@/type";
+import { RecommendationWithoutLogin, SearchResult, UnstoredResult, ValidatedRecommendation } from "@/type";
 import { handleDatabaseError } from "./activity";
 import { sendImgURLAndPromptToGPT, sendPromptToGPT } from "./utils/chat";
 import {
@@ -33,19 +33,25 @@ const handleRecommendation = async (
   imageUrl: string
 ): Promise<number> => {
   try {
-    const prompt = constructPromptForRecommendation({ clothingType, gender, numMaxSuggestion });
-    const recommendations = await sendImgURLAndPromptToGPT({ model, prompt, imageUrl });
+    let recommendations: string | null = null;
+    let cleanedRecommendations: ValidatedRecommendation[] = [];
 
-    if (!recommendations) return -1;
+    while (!recommendations || cleanedRecommendations.length === 0) {
+      const prompt = constructPromptForRecommendation({ clothingType, gender, numMaxSuggestion });
+      recommendations = await sendImgURLAndPromptToGPT({ model, prompt, imageUrl });
+      console.log("GPT recommendations= ", recommendations);
 
-    const cleanedRecommendations = validateLabelString(recommendations, clothingType);
+      if (!recommendations) continue;
+
+      cleanedRecommendations = validateLabelString(recommendations, clothingType);
+    }
     const uploadId: number = await insertUpload(imageUrl, userId);
     const paramId: number = await insertParam(gender, clothingType, model);
     const recommendationId: number = await insertRecommendation({
-        paramId,
-        uploadId,
-        userId,
-      });
+      paramId,
+      uploadId,
+      userId,
+    });
 
     await Promise.all(cleanedRecommendations.map(async (rec) => {
       const suggestionId = await insertSuggestion({
@@ -81,113 +87,98 @@ const handleRecommendationWithoutLogin = async (
   imageUrl: string
 ): Promise<RecommendationWithoutLogin[] | null> => {
   try {
-    const prompt = constructPromptForRecommendation({ clothingType, gender, numMaxSuggestion });
-    const rawLabelString: string | null = await sendImgURLAndPromptToGPT({ model, prompt, imageUrl });
+    let rawLabelString: string | null = null;
+    let cleanedLabels: ValidatedRecommendation[] = [];
 
-    if (rawLabelString) {
-      const cleanedLabels = validateLabelString(rawLabelString, clothingType);
+    while (!rawLabelString || cleanedLabels.length === 0) {
+      const prompt = constructPromptForRecommendation({ clothingType, gender, numMaxSuggestion });
+      rawLabelString = await sendImgURLAndPromptToGPT({ model, prompt, imageUrl });
 
-      if (cleanedLabels.length > 0) {
-        const recommendations: RecommendationWithoutLogin[] = [];
-
-        for (const cleanedLabel of cleanedLabels) {
-          const labelString = cleanedLabel.labelString;
-          const description = cleanedLabel.description;
-
-          const results = await semanticSearchWithoutLogin({
-            suggestedLabelString: labelString,
-            numMaxItem,
-            gender,
-            clothing_type: clothingType,
-          });
-
-          if (results) {
-            const recommendation: RecommendationWithoutLogin = {
-              clothing_type: clothingType,
-              gender: gender,
-              model: model,
-              image_url: imageUrl,
-              styles: {
-                default: {
-                  series: results,
-                  description: description,
-                },
-              },
-            };
-            recommendations.push(recommendation);
-          } else {
-            console.error("No results found in semanticSearchWithoutLogin for label:", labelString);
-          }
-        }
-        return recommendations;
-      } else {
-        console.error("No valid labels found after cleaning");
-        return null;
+      if (rawLabelString) {
+        cleanedLabels = validateLabelString(rawLabelString, clothingType);
       }
-    } else {
-      console.error("No label string returned from GPT");
-      return null;
+      console.log("GPT recommendations= ", cleanedLabels);
+
+      if (!rawLabelString || cleanedLabels.length === 0) {
+        console.warn("Retrying sendImgURLAndPromptToGPT due to invalid results...");
+      }
     }
+    const recommendations: RecommendationWithoutLogin[] = [];
+
+    for (const cleanedLabel of cleanedLabels) {
+      const labelString = cleanedLabel.labelString;
+      const description = cleanedLabel.description;
+
+      const results = await semanticSearchWithoutLogin({
+        suggestedLabelString: labelString,
+        numMaxItem,
+        gender,
+        clothing_type: clothingType,
+      });
+
+      if (results) {
+        const recommendation: RecommendationWithoutLogin = {
+          clothing_type: clothingType,
+          gender: gender,
+          model: model,
+          image_url: imageUrl,
+          styles: {
+            default: {
+              series: results,
+              description: description,
+            },
+          },
+        };
+        recommendations.push(recommendation);
+      } else {
+        console.error("No results found in semanticSearchWithoutLogin for label:", labelString);
+      }
+    }
+
+    return recommendations;
   } catch (error) {
     handleDatabaseError(error, "handleRecommendationWithoutLogin");
     return null;
   }
 };
 
-const handleImageSearch = async (
+const getLabelStringForImageSearch = async (
   gender: Gender,
   model: string,
-  imageUrl: string,
-  page: number,
-  priceLowerBound?: number,
-  priceUpperBound?: number,
-  providers?: string[],
-  clothingType?: ClothingType
-): Promise<SearchResult | null> => {
+  imageUrl: string
+): Promise<string> => {
   try {
-    const prompt: string = constructPromptForImageSearch({
-      gender,
-    });
+    let rawLabelString: string | null = null;
+    let cleanedLabels: ValidatedRecommendation[] = [];
 
-    const rawLabelString: string | null = await sendImgURLAndPromptToGPT({
-      model,
-      prompt,
-      imageUrl,
-    });
+    while (!rawLabelString || cleanedLabels.length === 0) {
+      const prompt: string = constructPromptForImageSearch({ gender });
 
-    if (rawLabelString) {
-      const cleanedLabels = validateLabelString(rawLabelString);
+      rawLabelString = await sendImgURLAndPromptToGPT({
+        model,
+        prompt,
+        imageUrl,
+      });
 
-      if (cleanedLabels.length > 0) {
-        const labelString = cleanedLabels[0].labelString;
-        const searchResult: SearchResult | null = await semanticSearchForSearching({
-          suggestedLabelString: labelString,
-          gender,
-          priceLowerBound,
-          priceUpperBound,
-          providers,
-          clothingType,
-          page,
-        });
-        console.log("searchResult: ", searchResult);
-        return searchResult;
-      } else {
-        console.error("No valid labels found after cleaning");
-        return null;
+      if (rawLabelString) {
+        cleanedLabels = validateLabelString(rawLabelString);
       }
-    } else {
-      console.error("No label string returned from GPT");
-      return null;
+      console.log("GPT recommendation: ", cleanedLabels);
+
+      if (!rawLabelString || cleanedLabels.length === 0) {
+        console.warn("Retrying sendImgURLAndPromptToGPT due to invalid results...");
+      }
     }
+
+    return cleanedLabels[0].labelString;
   } catch (error) {
-    handleDatabaseError(error, "handleImageSearch");
-    return null;
+    handleDatabaseError(error, "getLabelStringForImageSearch");
+    return "";
   }
 };
 
-const handleTextSearch = async (
-  query: string,
-  model: string,
+const handleImageSearch = async (
+  labelString: string,
   gender: Gender,
   page: number,
   priceLowerBound?: number,
@@ -196,48 +187,86 @@ const handleTextSearch = async (
   clothingType?: ClothingType,
 ): Promise<SearchResult | null> => {
   try {
-    const prompt: string = constructPromptForTextSearch({
-      query,
+    const searchResult: SearchResult | null = await semanticSearchForSearching({
+      suggestedLabelString: labelString,
       gender,
+      priceLowerBound,
+      priceUpperBound,
+      providers,
+      clothingType,
+      page,
     });
-
-    const rawLabelString: string | null = await sendPromptToGPT({
-      model,
-      prompt,
-    });
-
-    if (rawLabelString) {
-      const cleanedLabels = validateLabelString(
-        rawLabelString,
-      );
-
-      if (cleanedLabels.length > 0) {
-        const labelString = cleanedLabels[0].labelString;
-
-        const searchResult: SearchResult | null =
-          await semanticSearchForSearching({
-            suggestedLabelString: labelString,
-            gender,
-            priceLowerBound,
-            priceUpperBound,
-            providers,
-            clothingType,
-            page,
-          });
-        return searchResult;
-      } else {
-        console.error("No valid labels found after cleaning");
-        return null;
-      }
-    } else {
-      console.error("No label string returned from GPT");
-      return null;
-    }
+    console.log("searchResult: ", searchResult);
+    return searchResult;
   } catch (error) {
-    handleDatabaseError(error, "handleTextSearch");
+    handleDatabaseError(error, "handleImageSearch");
     return null;
   }
 };
 
-export { handleImageSearch, handleRecommendation, handleRecommendationWithoutLogin, handleTextSearch };
+const getLabelStringForTextSearch = async (
+  gender: Gender,
+  model: string,
+  query: string,
+): Promise<string> => {
+  try {
+    let rawLabelString: string | null = null;
+    let cleanedLabels: ValidatedRecommendation[] = [];
+
+    while (!rawLabelString || cleanedLabels.length === 0) {
+      const prompt: string = constructPromptForTextSearch({
+        query,
+        gender,
+      });
+
+      rawLabelString = await sendPromptToGPT({
+        model,
+        prompt,
+      });
+
+      if (rawLabelString) {
+        cleanedLabels = validateLabelString(rawLabelString);
+      }
+      console.log("GPT recommendation: ", cleanedLabels);
+
+      if (!rawLabelString || cleanedLabels.length === 0) {
+        console.warn("Retrying sendPromptToGPT due to invalid results...");
+      }
+    }
+
+    return cleanedLabels[0].labelString;
+  } catch (error) {
+    handleDatabaseError(error, "getLabelStringForTextSearch");
+    return "";
+  }
+};
+
+const handleTextSearch = async (
+  labelString: string,
+  gender: Gender,
+  page: number,
+  priceLowerBound?: number,
+  priceUpperBound?: number,
+  providers?: string[],
+  clothingType?: ClothingType,
+): Promise<SearchResult | null> => {
+  try {
+    const searchResult: SearchResult | null = await semanticSearchForSearching({
+      suggestedLabelString: labelString,
+      gender,
+      priceLowerBound,
+      priceUpperBound,
+      providers,
+      clothingType,
+      page,
+    });
+
+    return searchResult;
+  } catch (error) {
+    handleDatabaseError(error, "getSearchResultForTextSearch");
+    return null;
+  }
+};
+
+export { handleImageSearch, handleRecommendation, handleRecommendationWithoutLogin, handleTextSearch, getLabelStringForImageSearch, getLabelStringForTextSearch };
 
