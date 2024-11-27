3635b46bf54009d8fee2332f5a8e1e0a29ef1cf9
mchen127 <mchen127.p@gmail.com>
2024-11-27 14:47:11 +0800

    fix: fix sign up origin bug

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,11 +5,13 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { SubmitButton } from "./submit-button";
 
-export default function Login({
+export default async function Login({
   searchParams,
 }: {
-  searchParams: { message: string };
+  searchParams: Promise<{ message: string }>;
 }) {
+
+  const resolvedSearchParams = await searchParams;
   const signIn = async (formData: FormData) => {
     "use server";
 
@@ -30,7 +32,8 @@ export default function Login({
   const signUp = async (formData: FormData) => {
     "use server";
 
-    const origin = headers().get("origin");
+    const headersObject = await headers();
+    const origin = headersObject.get("origin");
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
@@ -53,43 +56,43 @@ export default function Login({
   };
 
   return (
-    <div className='flex items-center flex-col w-full px-8 sm:max-w-md justify-center gap-2 pt-8'>
-      <form className='animate-in flex flex-col w-full justify-center gap-2 text-foreground'>
-        <label className='text-md' htmlFor='email'>
+    <div className="flex items-center flex-col w-full px-8 sm:max-w-md justify-center gap-2 pt-8">
+      <form className="animate-in flex flex-col w-full justify-center gap-2 text-foreground">
+        <label className="text-md" htmlFor="email">
           電子郵件
         </label>
         <input
-          className='rounded-md px-4 py-2 bg-inherit border mb-6'
-          name='email'
-          placeholder='you@example.com'
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="email"
+          placeholder="you@example.com"
           required
         />
-        <label className='text-md' htmlFor='password'>
+        <label className="text-md" htmlFor="password">
           密碼
         </label>
         <input
-          className='rounded-md px-4 py-2 bg-inherit border mb-6'
-          type='password'
-          name='password'
-          placeholder='••••••••'
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          type="password"
+          name="password"
+          placeholder="••••••••"
           required
         />
-        {searchParams?.message && (
-          <p className='mt-4 p-4 bg-foreground/10 text-foreground text-center'>
-            {searchParams.message}
+        {resolvedSearchParams?.message && (
+          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+            {resolvedSearchParams.message}
           </p>
         )}
         <SubmitButton
           formAction={signIn}
-          className='bg-green-700 rounded-md px-4 py-2 text-foreground mb-2'
-          pendingText='Signing In...'
+          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+          pendingText="Signing In..."
         >
           登入
         </SubmitButton>
         <SubmitButton
           formAction={signUp}
-          className='border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2'
-          pendingText='Signing Up...'
+          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+          pendingText="Signing Up..."
         >
           註冊
         </SubmitButton>
