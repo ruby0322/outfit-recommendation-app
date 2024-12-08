d1ddeef03c2f4d5e7d17c0ae0661404784f2e857
Ruby <ruby0322@ntu.im>
2024-12-08 23:45:38 +0800

    feat: add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import TourCard from '@/components/tour-card';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
@@ -159,6 +160,7 @@ export default async function RootLayout({
         <Footer />
         <Toaster />
       </body>
+      <GoogleAnalytics gaId="G-79W179FVTX" />
     </html>
   );
 }
